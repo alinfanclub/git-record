5bff5e537a5c92302fe9a2757554294c285f646a
e759ksh <e759ksh@naver.com>
2023-02-14 17:47:08 +0900

    파비콘 등록 및 레이아웃 수정 팝업 텍스트 크기 수정

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -59,16 +59,16 @@ export default function PostList() {
         </ul>
       </div>
       {cookie[COOKIE_KEY] ? null : (
-        <div className="w-full h-full fixed top-0 left-0 bg-black/50 flex items-center justify-center">
-          <div className="w-5/6 sm:w-2/5 h-auto bg-white aspect-video relative p-4 rounded-lg">
+        <div className="w-full h-full fixed top-0 left-0 bg-black/50 flex items-center justify-center z-10">
+          <div className="w-5/6 sm:w-1/5 h-auto bg-white aspect-video relative p-4 rounded-lg">
             <div className="flex items-center justify-between">
-              <h4 className="text-base sd:text-2xl">공지 사항</h4>
+              <h4 className="text-base text-lg">공지 사항</h4>
               <AiFillCloseCircle
                 onClick={closeModal}
                 className="cursor-pointer text-3xl"
               />
             </div>
-            <div className="popup_text text-center flex flex-col h-full justify-center text-sm sd:text-xl">
+            <div className="popup_text text-center flex flex-col h-full justify-center text-sm text-xs">
               <p>안녕하세요, 부스러기입니다.</p>
               <p>
                 부스러기는 주로 시, 시각 예술을 수집하고 공유하는 사이트입니다.
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="border border-t-1 border-grey h-10 border-b-0 border-l-0 border-r-0 flex justify-center items-center mt-4">
-      <p className="text-sm text-stone-400">
-        Copyright © 2023 booseureogi. All Rights Reserved.
+    <footer className="border border-t-1 border-grey h-10 border-b-0 border-l-0 border-r-0 flex justify-center items-center mt-4  bg-white w-full translate-y-full sm:translate-y-0 sm:fixed bottom-0">
+      <p className="text-xs text-stone-400">
+        Copyright © 2023 Booseureogi. All Rights Reserved.
       </p>
-    </div>
+    </footer>
   );
 }

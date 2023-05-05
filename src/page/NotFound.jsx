57c0d334ce57537abc57a7434aba6f3228c60b52
e759ksh <e759ksh@naver.com>
2023-05-06 01:38:04 +0900

    404 페이지 업데이트

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SubmitButton from "../components/SubmitButton";
 
 export default function NotFound() {
-  return <div>not found page return please</div>;
+  return (
+    <section
+      className="flex items-center justify-center h-screen flex-col gap-[2rem] sm:gap-[4rem] px-4
+    "
+    >
+      <p className="sm:text-[10vw] text-6xl font-bold">Oops!</p>
+      <p className="text-2xl flex flex-col gap-4 items-center">
+        404 - Not Found Page
+        <smal className="text-sm text-neutral-500 align-middle">
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </smal>
+      </p>
+      <p>
+        <Link to="/">
+          <SubmitButton text={"Go Booseureogi !!"} />
+        </Link>
+      </p>
+    </section>
+  );
 }
